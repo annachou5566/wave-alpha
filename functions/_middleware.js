3671f25516269d6786b7e2e7ccc9d378de28c98c
annachou5566 <annachou60@gmail.com>
2026-02-12 09:10:37 +0700

    Update _middleware.js

--- functions/_middleware.js
+++ functions/_middleware.js
@@ -2,11 +2,8 @@ export async function onRequest(context) {
   const { request, env } = context;
   const url = new URL(request.url);
 
-  // Chỉ xử lý các yêu cầu lấy dữ liệu trong thư mục /data/
   if (url.pathname.startsWith('/data/')) {
 
-    // [QUAN TRỌNG] Cho phép trình duyệt hỏi đường (Preflight OPTIONS)
-    // Giúp sửa lỗi "TypeError: Failed to fetch" và lỗi CORS
     if (request.method === "OPTIONS") {
       return new Response(null, {
         status: 204,
@@ -19,13 +16,10 @@ export async function onRequest(context) {
       });
     }
 
-    // --- LỚP BẢO MẬT DUY NHẤT: KIỂM TRA HEADER BÍ MẬT ---
-    // Chúng ta bỏ qua bước check Referer vì nó hay gây lỗi cho người dùng thật.
     const customAuth = request.headers.get('X-Wave-Source');
     
-    // Nếu không có header 'web-client' -> CHẶN NGAY
     if (customAuth !== 'web-client') {
-         return new Response(JSON.stringify({ error: "Access Denied: Missing Secret Signature" }), { 
+         return new Response(JSON.stringify({ error: "Access Denied" }), { 
             status: 403,
             headers: { 
                 'Content-Type': 'application/json',
@@ -34,7 +28,6 @@ export async function onRequest(context) {
         });
     }
 
-    // --- LẤY FILE TỪ R2 ---
     let r2Key = null;
     if (url.pathname.includes('market-data.json')) r2Key = 'market-data.json';
     else if (url.pathname.includes('competition-history.json')) r2Key = 'competition-history.json';
@@ -48,7 +41,7 @@ export async function onRequest(context) {
         const headers = new Headers();
         object.writeHttpMetadata(headers);
         headers.set('etag', object.httpEtag);
-        headers.set("Access-Control-Allow-Origin", "*"); // Cho phép JS đọc được
+        headers.set("Access-Control-Allow-Origin", "*"); 
         headers.set('Cache-Control', 'private, max-age=30'); 
 
         return new Response(object.body, { headers });
