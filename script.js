c7562d3c544ac59317728ee9e65863f487d1f1ad
forgetme <annachou60@gmail.com>
2026-02-27 18:46:27 +0700

    Update script.js

--- script.js
+++ script.js
@@ -5,7 +5,7 @@
     const SUPABASE_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImFrYmNwcnlxamlnbmR6cHVvYW55Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjUwODg0NTEsImV4cCI6MjA4MDY2NDQ1MX0.p1lBHZ12fzyIrKiSL7DXv7VH74cq3QcU7TtBCJQBH9M';
     const REALTIME_API_URL = 'https://alpha-realtime.onrender.com/api/market-data';
     const REALTIME_API_KEY = 'WaveAlpha_S3cur3_P@ssw0rd_5566';
-
+    const supabase = window.supabase.createClient(SUPABASE_URL, SUPABASE_KEY);
 let layer2Interval = null;
 const PREDICT_FEE = 100;
 // --- KHAI BÁO BỔ SUNG CÁC BIẾN TOÀN CỤC ---
