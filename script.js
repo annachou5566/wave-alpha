2936e3b60961048dd00eb47cad053e1c2f9099ab
forgetme <annachou60@gmail.com>
2026-03-29 20:15:01 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,6 @@
     const SUPABASE_URL = 'https://akbcpryqjigndzpuoany.supabase.co';
     const SUPABASE_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImFrYmNwcnlxamlnbmR6cHVvYW55Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjUwODg0NTEsImV4cCI6MjA4MDY2NDQ1MX0.p1lBHZ12fzyIrKiSL7DXv7VH74cq3QcU7TtBCJQBH9M';
-    const REALTIME_API_URL = 'https://alpha-realtime.onrender.com/api/market-data';
-    const REALTIME_API_KEY = 'WaveAlpha_S3cur3_P@ssw0rd_5566';
+    const REALTIME_API_URL = '/api/market-data';
 window.supabase = window.supabase.createClient(SUPABASE_URL, SUPABASE_KEY);
 let layer2Interval = null;
 let wsSocket = null;            
